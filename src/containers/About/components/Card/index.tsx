f238d7ee69a5628807d99c8a0d31c532c7cfcba9
Connor Gawley (Desktop) <connorgawley@uvic.ca>
2022-11-28 20:31:33 -0800

    redid cards section and card component

--- src/containers/About/components/Card/index.tsx
+++ src/containers/About/components/Card/index.tsx
@@ -1,147 +1,134 @@
-import React from 'react'
-import './index.scss'
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { useState } from "react";
+import "./index.scss";
 
-interface Props {
-    category: 0 | 1 | 2 | 3
-    interests?: Interest[]
-    work?: Job[]
-    techSkills?: string[]
-    softSkills?: string[]
+export interface Props {
+    category: Types;
+    interests?: Interest[];
+    work?: Job[];
+    techSkills?: string[];
+    softSkills?: string[];
 }
 interface Interest {
-    title: string
-    desc: string
+    title: string;
+    desc: string;
 }
 interface Job {
-    title: string
-    subtitle: string
-    desc: string[]
+    title: string;
+    subtitle: string;
+    desc: string[];
 }
+
+export enum Types {
+    Interests,
+    WorkExperience,
+    TechnicalSkills,
+    SoftSkills,
+}
+
+const titles = ["Interests", "Work Experience", "Technical Skills", "Soft Skills"];
 
 const Card = (props: Props) => {
-    function toWords(num: number) {
-        switch (num) {
-            case 0:
-                return 'zero'
-            case 1:
-                return 'one'
-            case 2:
-                return 'two'
-            case 3:
-                return 'three'
-        }
-        return 'error'
+    const [parent] = useAutoAnimate<HTMLDivElement>();
+    const [showingFront, setShowingFront] = useState(true);
+
+    function getCardFront() {
+        return <h2>{titles[props.category]}</h2>;
     }
 
-    if (props.category == 0) {
-        // | INTERESTS |
-        if (!props.interests) return <></>
-        return (
-            <div className='card'>
-                <div className='front'>
-                    <h2 className='title'>Interests</h2>
-                </div>
-                <div className='back interest'>
-                    {props.interests &&
-                        props.interests.length > 0 &&
-                        props.interests.map((interest, index) => {
-                            let classNameStr = toWords(index) + ' content'
-                            return (
-                                <div key={interest.title} className={classNameStr}>
-                                    <h2 className='title'>{interest.title}</h2>
-                                    <p className='desc'>{interest.desc}</p>
-                                </div>
-                            )
-                        })}
-                </div>
-            </div>
-        )
-    }
-    if (props.category == 1) {
-        // | WORK EXP |
-        if (!props.work) return <></>
-        return (
-            <div className='card'>
-                <div className='front'>
-                    <h2 className='title'>Work Experience</h2>
-                </div>
-                <div className='back work'>
-                    {props.work &&
-                        props.work.length > 0 &&
-                        props.work.map((job, index) => {
-                            let classNameStr = toWords(index) + ' content'
-                            return (
-                                <div key={job.title} className={classNameStr}>
-                                    <h2 className='title'>{job.title}</h2>
-                                    <h3 className='subtitle'>{job.subtitle}</h3>
-                                    {job.desc &&
-                                        job.desc.length > 0 &&
-                                        job.desc.map((point) => {
-                                            return (
-                                                <p key={point}>
-                                                    {'🡒 '}
-                                                    {point}
-                                                </p>
-                                            )
-                                        })}
-                                </div>
-                            )
-                        })}
-                </div>
-            </div>
-        )
-    }
-    if (props.category == 2) {
-        // | TECH SKILLS |
-        if (!props.techSkills) return <></>
-        return (
-            <div className='card'>
-                <div className='front'>
-                    <h2 className='title'>Technical Skills</h2>
-                </div>
-                <div className='back tech-skills'>
-                    <ul>
-                        {props.techSkills &&
-                            props.techSkills.length > 0 &&
-                            props.techSkills.map((skill) => {
-                                return (
-                                    <li key={skill}>
-                                        {'✓ '}
-                                        {skill}
-                                    </li>
-                                )
-                            })}
-                    </ul>
-                </div>
-            </div>
-        )
-    }
-    if (props.category == 3) {
-        // | SOFT SKILLS |
-        if (!props.softSkills) return <></>
-        return (
-            <div className='card'>
-                <div className='front'>
-                    <h2 className='title'>Soft Skills</h2>
-                </div>
-                <div className='back soft-skills'>
-                    <ul>
-                        {props.softSkills &&
-                            props.softSkills.length > 0 &&
-                            props.softSkills.map((skill) => {
-                                return (
-                                    <li key={skill}>
-                                        {'✓ '}
-                                        {skill}
-                                    </li>
-                                )
-                            })}
-                    </ul>
-                </div>
-            </div>
-        )
-    }
-    return <></>
-}
+    function getInterests() {
+        if (!props.interests) return;
 
-export default Card
+        return (
+            <div>
+                {props.interests.map((interest) => {
+                    return (
+                        <div key={interest.title} className="content">
+                            <h2 className="title">{interest.title}</h2>
+                            <p>{interest.desc}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+
+    function getWorkExperience() {
+        if (!props.work) return;
+
+        return (
+            <div>
+                {props.work.map((job) => {
+                    return (
+                        <div key={job.title} className="content">
+                            <h2 className="title">{job.title}</h2>
+                            <h3 className="subtitle">{job.subtitle}</h3>
+                            {job.desc.map((point) => {
+                                return (
+                                    <p key={point}>
+                                        {"🡒 "}
+                                        {point}
+                                    </p>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+
+    function getTechSkills() {
+        if (!props.techSkills) return;
+
+        return (
+            <ul>
+                {props.techSkills.map((skill) => {
+                    return (
+                        <li key={skill}>
+                            {"✓ "}
+                            {skill}
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+
+    function getSoftSkills() {
+        if (!props.softSkills) return;
+
+        return (
+            <ul>
+                {props.softSkills.map((skill) => {
+                    return (
+                        <li key={skill}>
+                            {"✓ "}
+                            {skill}
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    }
+
+    return (
+        <div
+            className="card"
+            onClick={() => setShowingFront((showingFront) => !showingFront)}
+            ref={parent}>
+            {showingFront ? (
+                getCardFront()
+            ) : (
+                <div className="back">
+                    {props.category === Types.Interests && getInterests()}
+                    {props.category === Types.WorkExperience && getWorkExperience()}
+                    {props.category === Types.TechnicalSkills && getTechSkills()}
+                    {props.category === Types.SoftSkills && getSoftSkills()}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Card;
