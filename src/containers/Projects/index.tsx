7aa1fe27b2f0a8f5bb0c9948ffdbb4036871023a
Connor <connorgawley@yahoo.com>
2023-07-10 21:53:00 -0600

    updates

--- src/containers/Projects/index.tsx
+++ src/containers/Projects/index.tsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import './index.scss'
-import Project from './components/Project'
-import port from '../../images/website-thumbnail.jpg'
-import rw from '../../images/route-weather-thumbnail.jpg'
-import cf from '../../images/cocktail-website-thumbnail.jpg'
+import React from "react";
+import "./index.scss";
+import Project from "./components/Project";
+import rosewald from "../../images/rosewald-thumbnail.jpg";
+import nvim from "../../images/nvim-thumbnail.jpg";
+import cf from "../../images/cocktail-website-thumbnail.jpg";
 
 const Projects = () => {
-    return (
-        <div className='projects'>
-            <h1>These are some projects I have been working on:</h1>
-            <Project
-                img={port}
-                title='Portfolio'
-                desc='A web app to showcase my works'
-                tech={['HTML5', 'SCSS', 'JS']}
-                liveLink='https://gawlster.github.io/portfolio'
-                sourceLink='https://github.com/gawlster/portfolio'
-            />
+  return (
+    <div className="projects">
+      <h1>These are some projects I have been working on:</h1>
+      <Project
+        img={rosewald}
+        title="Rosewald Studios"
+        desc="A portfolio site for Rosewald Studios, a local photography studio"
+        tech={["NextJS", "Tailwind"]}
+        liveLink="https://rosewaldstudios.ca"
+        sourceLink="https://github.com/gawlster/rosewald"
+      />
 
-            <Project
-                img={rw}
-                title='Route Weather'
-                desc='A web app to find weather along a route'
-                tech={['ReactJS', 'SCSS', 'REST API']}
-                liveLink='https://gawlster.github.io/portfolio/request-access.html'
-                sourceLink='https://gawlster.github.io/portfolio/request-access.html'
-            />
+      <Project
+        img={nvim}
+        title="Nvim config"
+        desc="A custom Neovim configuration consisting of plugins and custom scripts"
+        tech={["Lua", "VimScript"]}
+        onlySourceCodeLink="https://github.com/gawlster/nvim"
+      />
 
-            <Project
-                img={cf}
-                title='Cocktail Finder'
-                desc='A web app to find a perfect cocktail'
-                tech={['HTML5', 'SCSS', 'JS']}
-                liveLink='https://webhome.csc.uvic.ca/~connorgawley/project2'
-                sourceLink='https://github.com/gawlster/cocktail-website'
-            />
-            <a href='https://github.com/gawlster' className='button'>
-                More of my projects
-            </a>
-        </div>
-    )
-}
+      <Project
+        img={cf}
+        title="Cocktail Finder"
+        desc="A webapp to explore and search for cocktail recipes"
+        tech={["HTML5", "SCSS", "JS"]}
+        liveLink="https://webhome.csc.uvic.ca/~connorgawley/project2"
+        sourceLink="https://github.com/gawlster/cocktail-website"
+      />
+      <a href="https://github.com/gawlster" className="button">
+        More of my projects
+      </a>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
