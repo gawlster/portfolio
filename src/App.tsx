85faa086a67a3c86ff2af8d417ffa4578861e393
gawlster <connorgawley@gmail.com>
2025-07-09 15:04:03 -0700

    Add vercel analytics package and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { Analytics } from "@vercel/analytics/react";
 import Contact from "./pages/Contact";
 import Homepage from "./pages/Homepage";
 import Projects from "./pages/Projects";
 import Qualifications from "./pages/Qualifications";
 
 function App() {
-  return (
-    <>
-      <Homepage />
-      <Projects />
-      <Qualifications />
-      <Contact />
-    </>
-  );
+    return (
+        <>
+            <Analytics />
+            <Homepage />
+            <Projects />
+            <Qualifications />
+            <Contact />
+        </>
+    );
 }
 
 export default App;
